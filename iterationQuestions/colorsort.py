0e3f9c78ae722a52560348afb1e9b8fe7f2bb03f
pravar-more <more.pravar@gmail.com>
2025-01-14 19:52:17 +0530

    nm

---
+++ iterationQuestions/colorsort.py
@@ -0,0 +1,35 @@
+'''
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+ 
+
+'''
+#SOLUTION 1
+
+# Method 3: In-Place Swap
+# Use in-place swapping to ensure elements are positioned correctly.
+
+def sortColors(self,nums):
+    red,white,blue = 0,0,len(nums)-1
+    
+
+
+
+#SOLUTION 2
+
+
+#SOLUTION 3
+
+
+#SOLUTION 4
+
+
+#MAIN
+
+nums = [2, 0, 2, 1, 1, 0] 
+sortColors(nums) 
+print(nums) # Output will be [0, 0, 1, 1, 2, 2]
